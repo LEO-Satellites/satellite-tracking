d51eb130efd6ca988ec6a8d9b2bfd6c6890cef71
Edgar Ortiz <ed.ortizm@gmail.com>
2021-08-14 14:46:05 -0400

    testing branching

--- satellite_visibility.py
+++ satellite_visibility.py
@@ -21,19 +21,11 @@ from lib_satellite_track import compute_visible, download_tle
 if __name__ == '__main__':
     ############################################################################
     ti = time.time()
-    ################################################################################
+    ############################################################################
     parser = ConfigParser()
     parser.read('configuration.ini')
     ############################################################################
-    satellite_brand = parser.get('user configuration', 'satellite_brand')
-    observatory = parser.get('user configuration', 'observatory')
-
-    day = parser.getint('user configuration', 'day')
-    month = parser.getint('user configuration', 'month')
-    year = parser.getint('user configuration', 'year')
-
-    window = parser.get('user configuration', 'window')
-    ############################################################################
+    # writing results
     output_dir = parser.get('paths', 'output_dir')
     data_output_dir = parser.get('paths', 'data_output_dir')
     if not os.path.exists(data_output_dir):
@@ -42,15 +34,17 @@ if __name__ == '__main__':
     output_fname = (f'visible-{satellite_brand}s_{observatory}_'
                     f'{year}_{month:02}_{day:02}_{window}')
 
-    output_fname_simple = (f'observing-details_for-{satellite_brand}_'
-      f'{observatory}_{year}_{month:02}_{day:02}_{window}')
-
     with open(f'{data_output_dir}/{output_fname}.txt', 'w') as file:
         file.write(f'{colum_headers}\n')
+    ############################################################################
+    output_fname_simple = (f'observing-details_for-{satellite_brand}_'
+      f'{observatory}_{year}_{month:02}_{day:02}_{window}')
 
     with open(f'{data_output_dir}/{output_fname_simple}.txt', 'w') as file:
         file.write(f'Date(UT)\tTime(UT)\tRA(hh:mm:ss)\tDEC(dd:mm:ss)\n')
     ############################################################################
+    # downloading tle file
+    ## update this block to use pandas DataFrame :)
     tle_dir = parser.get('paths', 'tle_dir')
     tle_fname = download_tle(satellite_brand, tle_dir)
     tle_file_path = f'{tle_dir}/{tle_fname}'
@@ -66,7 +60,15 @@ if __name__ == '__main__':
                 satellites_list.append(l.strip())
     ############################################################################
     observatories = get_observatory_data(observatories)
+    satellite_brand = parser.get('user configuration', 'satellite_brand')
+    observatory = parser.get('user configuration', 'observatory')
     observatory_data = observatories[observatory]
+    ############################################################################
+    day = parser.getint('user configuration', 'day')
+    month = parser.getint('user configuration', 'month')
+    year = parser.getint('user configuration', 'year')
+
+    window = parser.get('user configuration', 'window')
     ############################################################################
     sat_alt_lower_bound = parser.getfloat('satellite observing limits',
                                         'sat_alt_lower_bound')
@@ -74,8 +76,11 @@ if __name__ == '__main__':
     ############################################################################
     sun_zenith_range = parser.get('satellite observing limits',
                                   'sun_zenith_angle_range')
+
     sun_zenith_lower, sun_zenith_upper = sun_zenith_range.split(',')
-    sun_zenith_lower, sun_zenith_upper = float(sun_zenith_lower), float(sun_zenith_upper)
+
+    (sun_zenith_lower, sun_zenith_upper) = (float(sun_zenith_lower),
+                                            float(sun_zenith_upper))
     ############################################################################
     compute_visible_parallel = partial(compute_visible,
                                        window=window,
@@ -89,17 +94,17 @@ if __name__ == '__main__':
                                        sun_zenith_upper=sun_zenith_upper)
 
     with mp.Pool(processes=None) as pool:
-        res = pool.map(compute_visible_parallel, satellites_list)
-################################################################################
+        results = pool.map(compute_visible_parallel, satellites_list)
+    ############################################################################
+    # retrieving visible satellites
     visible_satellites = []
-
-    for satellite in res:
+    for satellite in results:
 
         if satellite == None:
             continue
 
         visible_satellites.append(satellite)
-################################################################################
+    ############################################################################
     observing_time = np.empty(len(visible_satellites))
 
     for idx, visible in enumerate(visible_satellites):
@@ -118,20 +123,21 @@ if __name__ == '__main__':
         obs_time = hours + minutes + seconds
 
         observing_time[idx] = obs_time
-################################################################################
+    ############################################################################
     obs_time_sort_ids = np.argsort(observing_time)
 
     for sort_id in obs_time_sort_ids:
 
         [satellite, data_str, data_str_simple] = visible_satellites[sort_id]
 
-        with open(f'{data_output_dir}/{output_fname}.txt',
-                  'a') as file:
+        with open(f'{data_output_dir}/{output_fname}.txt','a') as file:
+
             file.write(f'{satellite}\n{data_str}\n')
 
-        with open(f'{data_output_dir}/{output_fname_simple}.txt',
-                  'a') as file_simple:
-            file_simple.write(f'{satellite}\n{data_str_simple}\n')
+        with open(f'{data_output_dir}/{output_fname_simple}.txt', 'a') as file:
+
+            file.write(f'{satellite}\n{data_str_simple}\n')
 ################################################################################
     tf = time.time()
     print(f'Running time: {tf-ti:.2} [s]')
+# this is a test to see what happens with the branches
