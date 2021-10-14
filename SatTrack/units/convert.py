b8454176e353e50619cf3dcdfb9cc8d448821d89
Edgar Ortiz <ed.ortizm@gmail.com>
2021-10-14 11:47:58 -0300

    Implemnt ConvertUnits class partially with documentation ;)

--- SatTrack/units/convert.py
+++ SatTrack/units/convert.py
@@ -1,6 +1,67 @@
 import numpy as np
 
-################################################################################
+###############################################################################
+"""
+RA: right ascention
+DEc: declination
+"""
+###############################################################################
+class ConvertUnits:
+    def __init__(self):
+        pass
+
+    def RA_in_radians_to_hours(self, RA: "float")->"float":
+        """
+        Converts RA from degress to hours
+
+        PARAMETERS
+            RA: RA in radians ??
+
+        OUTPUTS
+            RA_in_hours: RA in hours
+
+        """
+        RA_in_degrees = self.radians_to_degrees(RA)
+
+        if RA_in_degrees < 0:
+            RA_in_degrees += 360
+
+        RA_in_hours = RA_in_degrees * (24.0 / 360.0)
+
+        return RA_in_hours
+    ###########################################################################
+    def radians_to_degrees(self, radians: "float")->"float":
+        """Convert radians to degrees"""
+
+        degrees = radians * 180.0 / np.pi
+
+        return degrees
+
+    ###########################################################################
+    def RA_in_radians_to_hh_mm_ss(self, RA: "float")->"list":
+        """
+        Converts RA in radians to hh:mm:ss.sss
+
+        PARAMETERS
+
+        OUTPUTS
+            [hh, mm, ss]
+                hh: int value of hours
+                mm: int value of minutes
+                ss: int value of seconds
+        """
+
+        hours = self.RA_in_radians_to_hours(RA)
+
+        hh = int(hours)
+
+        minutes = (hours - hh) * 60.0
+        mm = int(minutes)
+
+        ss = (minutes - mm) * 60
+
+        return [hh, mm, ss]
+###############################################################################
 def ra_to_hours(ra):
     ra = ra * 180.0 / np.pi
 
