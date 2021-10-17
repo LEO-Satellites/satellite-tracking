8d0b835dce5fe738ceda26402a53e467b3d226f1
Edgar Ortiz <ed.ortizm@gmail.com>
2021-10-17 11:06:07 -0300

    Corect output format in RA and DEc.RA: 12:22:8.546 --> 12:22:08.55. DEC: 025:16:39.286 --> +25:16:39.286

--- SatTrack/format/format.py
+++ SatTrack/format/format.py
@@ -29,17 +29,13 @@ def data_formating(
     data = [
         f"{date}",
         f"{time}",
-        # SatLon[deg]
         f"{darksat_latlon[0]:9.6f}",
-        # SatLat[deg]
         f"{darksat_latlon[1]:9.6f}",
         f"{darksat_latlon[2]:5.2f}",
         f"{sat_az:06.3f}",
         f"{sat_alt:06.3f}",
-        # f"{raSAT_h:02d}h{raSAT_m:02d}m{raSAT_s:05.3f}s",
-        # f"{decSAT_d:03d}:{decSAT_m:02d}:{decSAT_s:05.3f}",
-        f"{raSAT_h:02d}:{raSAT_m:02d}:{raSAT_s:05.3f}",
-        f"{decSAT_d:03d}:{decSAT_m:02d}:{decSAT_s:05.3f}",
+        f"{raSAT_h:02d}:{raSAT_m:02d}:{raSAT_s:05.2f}",
+        f"{decSAT_d:+03d}:{decSAT_m:02d}:{decSAT_s:05.2f}",
         f"{sunRA:09.7f}",
         f"{sunDEC:09.7f}",
         f"{sun_zenith_angle:07.3f}",
@@ -49,9 +45,8 @@ def data_formating(
     data_simple = [
         f"{date}",
         f"{time}",
-        # f"{raSAT_h:02d}h{raSAT_m:02d}m{raSAT_s:05.3f}s",
-        f"{raSAT_h:02d}:{raSAT_m:02d}:{raSAT_s:05.3f}",
-        f"{decSAT_d:03d}:{decSAT_m:02d}:{decSAT_s:05.3f}",
+        f"{raSAT_h:02d}:{raSAT_m:02d}:{raSAT_s:05.2f}",
+        f"{decSAT_d:+03d}:{decSAT_m:02d}:{decSAT_s:05.2f}",
     ]
 
     return data, data_simple
