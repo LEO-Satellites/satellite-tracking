f8059afc652b2c7028f2cafbb4829a8c031d08ab
Edgar Ortiz <ed.ortizm@gmail.com>
2021-10-28 12:14:29 -0300

    Delete configuration section from track.ini

--- SatTrack/visible.py
+++ SatTrack/visible.py
@@ -133,37 +133,12 @@ class ComputeVisibility(FileDirectory):
             self._update_observer_date(date_time)
 
             [
-                [
-                    ra_satellite_h,
-                    ra_satellite_m,
-                    ra_satellite_s
-                ],
-                [
-                    dec_satellite_d,
-                    dec_satellite_m,
-                    dec_satellite_s
-                ]
+                [ra_satellite_h, ra_satellite_m, ra_satellite_s],
+                [dec_satellite_d, dec_satellite_m, dec_satellite_s]
             ] = self._get_satellite_RA_DEC_from_azimuth_and_altitude(
                     satellite_azimuth,
                     satellite_altitude
                 )
-            #
-            # RA_satellite, DEC_satellite = self.observer.radec_of(
-            #     np.radians(satellite_azimuth),
-            #     np.radians(satellite_altitude)
-            # )
-            # ##################################################################
-            # [
-            #     ra_satellite_h,
-            #     ra_satellite_m,
-            #     ra_satellite_s
-            # ]= convert.RA_in_radians_to_hh_mm_ss(RA=RA_satellite)
-            #
-            # [
-            #     dec_satellite_d,
-            #     dec_satellite_m,
-            #     dec_satellite_s
-            # ]= convert.DEC_in_radians_to_dd_mm_ss(DEC=DEC_satellite)
             ###################################################################
         #     visible = (satellite_altitude > satellite_altitude_lower_bound) and (
         #         sun_zenith_lower < sun_zenith_angle < sun_zenith_upper
@@ -234,16 +209,8 @@ class ComputeVisibility(FileDirectory):
         ]= convert.DEC_in_radians_to_dd_mm_ss(DEC=DEC_satellite)
 
         return [
-            [
-                ra_satellite_h,
-                ra_satellite_m,
-                ra_satellite_s
-            ],
-            [
-                dec_satellite_d,
-                dec_satellite_m,
-                dec_satellite_s
-            ]
+            [ra_satellite_h, ra_satellite_m, ra_satellite_s],
+            [dec_satellite_d, dec_satellite_m, dec_satellite_s]
         ]
     ###########################################################################
     def _set_time_parameters(self, time_parameters: "dictionary"):
